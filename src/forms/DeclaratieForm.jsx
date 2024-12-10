002ab645aed685595a759c646bfb373aca75ed45
shaneallmighty <shaneokallahan@hotmail.com>
2024-12-10 12:13:08 +0100

    deployment test

--- src/forms/DeclaratieForm.jsx
+++ src/forms/DeclaratieForm.jsx
@@ -1,6 +1,6 @@
 import FormInput from "../components/FormInput";
 import { useState, useRef } from "react";
-import { CSVLink,} from "react-csv";
+import { CSVLink } from "react-csv";
 import "../css/DeclaratieForm.css";
 
 function DeclaratieForm() {
@@ -28,17 +28,28 @@ function DeclaratieForm() {
       return;
     }
     csvLinkRef.current.link.click(); // Programmatically click the CSVLink
-    setShowCheck(true);//Show checkmark
+    setShowCheck(true); //Show checkmark
   };
 
-const addDeclaration = (e) => {
-  e.preventDefault();
-  if (!naamOrg || !datumBon || !bedrag) {
-    alert("Please fill in all fields.");
-    return;
-  }
-}
+  // When clicking the add button, add a declaration to the list
+  const addDeclaration = (e) => {
+    e.preventDefault();
+    if (!naamOrg || !datumBon || !bedrag) {
+      alert("Please fill in all fields.");
+      return;
+    }
+    const newDeclaration = {
+      naamOrg,
+      datumBon,
+      bedrag,
+    };
+    setDeclarations([...declarations, newDeclaration]);
 
+    // Clear input fields
+    setNaamOrg("");
+    setDatumBon("");
+    setBedrag("");
+  };
 
   // Define formatted CSV data with a single header row and blank rows for spacing
   const formattedData = [
@@ -51,89 +62,116 @@ const addDeclaration = (e) => {
     ["", "", "", ""],
     ["", "", "", ""],
     ["", "", "", ""],
+    ["", "", "", ""],
     ["Naam organisatie", "Datum bon", "Bedrag inclusief BTW"],
+    ...declarations.map((decl) => [decl.naamOrg, decl.datumBon, decl.bedrag]),
   ];
 
   return (
     <div className="inner-card">
-    <div className="personalInfo">
-    <form onSubmit={handleDownload} className="form-personal-info">
-      <FormInput
-        placeholder="Datum"
-        setInput={setDatum}
-        value={datum}
-        type=""
-      />
-      <FormInput placeholder="Naam" setInput={setNaam} value={naam} type="" />
-      <FormInput
-        placeholder="Bankrekeningnummer"
-        setInput={setBankrek}
-        value={bankrek}
-        type=""
-      />
-      <FormInput
-        placeholder="Totaal bedrag"
-        setInput={setTotaal}
-        value={totaal}
-        type=""
-      />
-
-      <div>
-      <button className="button-download"
-      type="button"
-      onClick={handleDownload}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          width="10" 
-          height="10" 
-          style={{ display: "inline-block" }}
-        >
-          <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-        </svg>
-        Download
-      </button>
-        {showCheck && (
-          <img
-            src={`${process.env.PUBLIC_URL}/check.png`}
-            alt="check"
-            className="check-image"
+      <div className="personalInfo">
+        <form onSubmit={handleDownload} className="form-personal-info">
+          <FormInput
+            placeholder="Datum"
+            setInput={setDatum}
+            value={datum}
+            type=""
           />
-        )}
+          <FormInput
+            placeholder="Naam"
+            setInput={setNaam}
+            value={naam}
+            type=""
+          />
+          <FormInput
+            placeholder="Bankrekeningnummer"
+            setInput={setBankrek}
+            value={bankrek}
+            type=""
+          />
+          <FormInput
+            placeholder="Totaal bedrag"
+            setInput={setTotaal}
+            value={totaal}
+            type=""
+          />
+
+          <div>
+            <button
+              className="button-download"
+              type="button"
+              onClick={handleDownload}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                width="10"
+                height="10"
+                style={{ display: "inline-block" }}
+              >
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
+              Download
+            </button>
+            {showCheck && (
+              <img
+                src={`${process.env.PUBLIC_URL}/check.png`}
+                alt="check"
+                className="check-image"
+              />
+            )}
+          </div>
+
+          <CSVLink
+            ref={csvLinkRef}
+            data={formattedData}
+            separator=";"
+            filename={`Declaratie_${naam}_${datum}`}
+            style={{ display: "none" }}
+          >
+            Download me
+          </CSVLink>
+        </form>
       </div>
+      <div className="declaraties">
+        <form onSubmit={addDeclaration} className="form-declaratie">
+          <FormInput
+            placeholder="Naam organisatie"
+            setInput={setNaamOrg}
+            value={naamOrg}
+            type=""
+          />
 
-      <CSVLink
-      ref={csvLinkRef} 
-          data={formattedData}
-          separator=";"
-          filename={`Declaratie_${naam}_${datum}`}
-          style={{ display: "none" }}
-        >Download me</CSVLink>
-    </form>
-    </div>
-    <div className="declaraties">
-      <form onSubmit={addDeclaration} className = "form-declaratie">
-        <FormInput
-          placeholder="Naam organisatie"
-          setInput={setNaamOrg}
-          value={naamOrg}
-          type=""/>
-        
-        <FormInput
-          placeholder="Datum bon"
-          setInput={setDatumBon}
-          value={datumBon}
-          type="date"/>
+          <FormInput
+            placeholder="Datum bon"
+            setInput={setDatumBon}
+            value={datumBon}
+            type="date"
+          />
 
-        <FormInput
-          placeholder="Bedrag inclusief BTW"
-          setInput={setBedrag}
-          value={bedrag}
-          type="number"/>
-
-        </form>    
-      
-        </div>
+          <FormInput
+            placeholder="Bedrag inclusief BTW"
+            setInput={setBedrag}
+            value={bedrag}
+            type="number"
+          />
+        </form>
+        <button class="button-add" type="button" onClick={addDeclaration}>
+          <img
+            src={`${process.env.PUBLIC_URL}/plus.png`}
+            alt="plus"
+            className="button-plus"
+            style={{ width: "15px", height: "15px" }}
+          />
+        </button>
+        <ul className="declaration-list">
+          {declarations.map((decl, index) => (
+            <li key={index}>
+              {decl.naamOrg}, {decl.datumBon}, €{decl.bedrag}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
